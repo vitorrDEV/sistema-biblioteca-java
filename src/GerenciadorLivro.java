0308da180601c0060a19a0ea18245522d760069a
vitorrDEV <vitorrmendes45@gmail.com>
2026-06-09 18:42:23 -0300

    Menu criado

--- src/GerenciadorLivro.java
+++ src/GerenciadorLivro.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class GerenciadorLivro {
+
+    ArrayList<Livro> livros = new ArrayList<>();
+
+
+    public void cadastrarLivro(Livro livro){
+        livros.add(livro);
+        System.out.println("Livro: "+livro.getTitulo()+" cadastrado com sucesso");
+    }
+
+    public void listarLivros(){
+            if (livros.isEmpty()){
+                System.out.println("Nenhum livro cadastrado ainda.");
+            }
+            System.out.println("---Lista de Livros Cadastrados ---");
+            for (Livro l: livros){
+                System.out.println("Titulo: "+l.getTitulo());
+                System.out.println("Autor: "+l.getAutor());
+                System.out.println("Ano: "+l.getAno());
+            }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        GerenciadorLivro gerenciadorLivro = new GerenciadorLivro();
+        int op = 0;
+
+        do {
+            System.out.println("""
+                    ==========================
+                       SISTEMA DE BIBLIOTECA
+                    ==========================
+                    
+                    1 - Cadastrar Livro
+                    2 - Listar Livros
+                    3 - Emprestar Livro
+                    4 - Devolver Livro
+                    5 - Buscar Livro
+                    6 - Remover Livro
+                    0 - Sair
+                    """);
+
+            System.out.println("Escolha uma opção");
+            System.out.print(">");
+            op = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (op){
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 0:
+                    System.out.println("Encerrando...");
+                    break;
+                default:
+                    System.out.println("Digite uma opção válida!!!");
+            }
+        } while (op != 0);
+    }
 }
