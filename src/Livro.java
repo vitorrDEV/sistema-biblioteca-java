83c8e069cd27c46e0bc2e02feed5ea517330db55
vitorrDEV <vitorrmendes45@gmail.com>
2026-06-09 18:23:33 -0300

    Classes Criadas

---
+++ src/Livro.java
@@ -0,0 +1,36 @@
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private int ano;
+
+    public Livro(String titulo, String autor, int ano) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
